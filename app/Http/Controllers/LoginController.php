8e1f82ac453b86f553d725a1fd2bf0ddd02c8dc5
alper-baydeniz <106283309+baydeniz18@users.noreply.github.com>
2024-04-16 20:42:28 +0300

    login will be added to route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -37,4 +37,28 @@ class LoginController extends Controller
 
         return response($response,201);
     }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Kullanıcı Adı - Şifre Hatalı'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
