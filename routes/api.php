ef83a0ac07984b80e834bfae4b9a0ac939f7af6e
alper-baydeniz <106283309+baydeniz18@users.noreply.github.com>
2024-04-16 19:46:56 +0300

    1

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,19 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BlogController;
 
 Route::get('/category_list', [CategoryController::class, 'index']);
+Route::get('/blog_list',[BlogController::class,'index']);
 Route::post('/add_blog',[BlogController::class,'store']);
 
-Route::group(['prefix' => 'blog'], function () {
-    Route::get('/blog_list', [BlogController::class, 'index']);
+// Route::group(['prefix' => 'blog'], function () {
+//     Route::get('/blog_list', [BlogController::class, 'index']);
 
-    Route::post('/add_blog',[BlogController::class,'store']);
+//     Route::post('/add_blog',[BlogController::class,'store']);
+// });
+
+
+Route::any('/', function () {
+    return response()->json([
+        'S'=>'H',
+        'message' => 'Route Yanlış.'
+    ]);
 });
