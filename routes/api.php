d3f93b8831663ed4f0dbba6d2e5286c8c618b4c3
alper-baydeniz <106283309+baydeniz18@users.noreply.github.com>
2024-04-16 20:44:25 +0300

    1

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LoginController;
 
+
+//USER
 Route::post('/register',[LoginController::class,'register']);
+Route::post('/login',[LoginController::class,'login']);
 
-Route::post('/add_blog',[BlogController::class,'store']);
 
-Route::get('/category_list', [CategoryController::class, 'index']);
+//PUBLIC LISTS
 Route::get('/blog_list',[BlogController::class,'index']);
+
+//NEED TOKEN
+Route::post('/add_blog',[BlogController::class,'store']);
+Route::get('/category_list', [CategoryController::class, 'index']);
+
